7b80cb2806f3ba87585c5459fa609e55cedd0a56
bhanu-shekar <bhanushekar.reddy@vithiit.com>
2026-02-11 12:50:21 +0530

    Added sidebar

---
+++ components/ui/app-sidebar.tsx
@@ -0,0 +1,58 @@
+import { Bot, Sparkles, Wrench } from "lucide-react";
+import Link from "next/link";
+
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarGroup,
+  SidebarGroupContent,
+  SidebarGroupLabel,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+} from "@/components/ui/sidebar";
+
+// Menu items.
+const items = [
+  {
+    title: "Agents",
+    url: "/agents",
+    icon: Bot,
+  },
+  {
+    title: "Tools",
+    url: "/tools",
+    icon: Wrench,
+  },
+  {
+    title: "Assistants",
+    url: "/assistants",
+    icon: Sparkles,
+  },
+];
+
+export function AppSidebar() {
+  return (
+    <Sidebar>
+      <SidebarContent>
+        <SidebarGroup>
+          <SidebarGroupLabel>Vgen Dashboard</SidebarGroupLabel>
+          <SidebarGroupContent>
+            <SidebarMenu>
+              {items.map((item) => (
+                <SidebarMenuItem key={item.title}>
+                  <SidebarMenuButton asChild>
+                    <Link href={item.url}>
+                      <item.icon />
+                      <span>{item.title}</span>
+                    </Link>
+                  </SidebarMenuButton>
+                </SidebarMenuItem>
+              ))}
+            </SidebarMenu>
+          </SidebarGroupContent>
+        </SidebarGroup>
+      </SidebarContent>
+    </Sidebar>
+  );
+}
